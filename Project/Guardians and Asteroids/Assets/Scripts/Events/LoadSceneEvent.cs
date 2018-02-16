b287b6662b074006ba63a994c911a699e18daacb
Sebastian <sebastian@stryczek.pl>
2018-02-16 18:22:15 +0100

    Create scene loading event

---
+++ Project/Guardians and Asteroids/Assets/Scripts/Events/LoadSceneEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using Object = UnityEngine.Object;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "LoadSceneEvent", menuName = "GameEvents/LoadSceneEvent", order = 0)]
+public class LoadSceneEvent : ScriptableObject
+{
+    public void LoadScene(Object obj)
+    {
+        SceneManager.LoadScene(obj.name);
+    }
+}
